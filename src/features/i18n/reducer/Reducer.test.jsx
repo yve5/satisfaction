9ba2cacb96b74b5b13b75dd2cdcac32cc4ea602a
Yves ROUILLÉ <yves.rouille@orange.com>
2021-03-23 12:40:02 +0100

    feat. i18n

--- src/features/i18n/reducer/Reducer.test.jsx
+++ src/features/i18n/reducer/Reducer.test.jsx
@@ -1,79 +1,59 @@
 import reducer from './Reducer';
-import { changeLang } from '../actions/All';
+import { changeLang, initSession } from '../actions/All';
 
 const frData = {
   id: 1,
   lang: 'fr',
   'Orange is a French telecommunications company.':
-    'Orange est une entreprise française de télécommunications.'
+    'Orange est une entreprise française de télécommunications.',
 };
 const enData = {
   id: 2,
   lang: 'en',
   'Orange is a French telecommunications company.':
-    'Orange is a French telecommunications company.'
+    'Orange is a French telecommunications company.',
 };
 const ruData = {
   id: 3,
   lang: 'ru',
   'Orange is a French telecommunications company.':
-    'Оранж - французская телекоммуникационная компания.'
+    'Оранж - французская телекоммуникационная компания.',
 };
 
 describe('The reducer', () => {
+  const initialState = {
+    collection: [frData, enData, ruData],
+    dictionary: frData,
+    lang: 'fr',
+  };
+
   it('should handle initial state', () => {
     expect(reducer(undefined, {})).toHaveProperty('collection');
     expect(reducer(undefined, {})).toHaveProperty('dictionary');
   });
 
-  it('should handle I18N_CHANGE_LANG. Part 1', () => {
-    expect(
-      reducer(
-        {
-          collection: [frData, enData, ruData],
-          dictionary: frData
-        },
-        changeLang('en')
-      )
-    ).toEqual({
+  it('should handle I18N_CHANGE_LANG. en', () => {
+    expect(reducer(initialState, changeLang('en'))).toEqual({
+      ...initialState,
       type: 'I18N_CHANGE_LANG',
-      collection: [frData, enData, ruData],
       dictionary: enData,
-      lang: 'en'
+      lang: 'en',
     });
   });
 
-  it('should handle I18N_CHANGE_LANG. Case 2', () => {
-    expect(
-      reducer(
-        {
-          collection: [frData, enData, ruData],
-          dictionary: ruData
-        },
-        changeLang('fr')
-      )
-    ).toEqual({
+  it('should handle I18N_CHANGE_LANG. fr', () => {
+    expect(reducer(initialState, changeLang('fr'))).toEqual({
+      ...initialState,
       type: 'I18N_CHANGE_LANG',
-      collection: [frData, enData, ruData],
-      dictionary: frData,
-      lang: 'fr'
     });
   });
 
-  it('should handle I18N_CHANGE_LANG. Case 3', () => {
-    expect(
-      reducer(
-        {
-          collection: [frData, enData, ruData],
-          dictionary: enData
-        },
-        changeLang('ru')
-      )
-    ).toEqual({
+  it('should handle I18N_CHANGE_LANG. ru', () => {
+    expect(reducer(initialState, changeLang('ru'))).toEqual({
+      ...initialState,
       type: 'I18N_CHANGE_LANG',
-      collection: [frData, enData, ruData],
       dictionary: ruData,
-      lang: 'ru'
+      lang: 'ru',
     });
   });
 });
