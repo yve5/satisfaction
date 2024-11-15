0f1baa2777db40b93d203b0826ea490e9dad300a
yve5 <yves.rouille@gmail.com>
2024-11-15 18:48:06 +0100

    wip

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Hello from 'react-dom';
 import { Provider, ReactReduxContext } from 'react-redux';
 
-import store from './config/store';
-import App from './satisfaction';
+// import * as All from 'organe/title';
+
+// import store from './config/store';
+// import App from './satisfaction';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App context={ReactReduxContext} />
-  </Provider>
-);
+console.log(Hello);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <App context={ReactReduxContext} />
+//   </Provider>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
